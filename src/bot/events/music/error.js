2a5283caa3217495ca0b0e730bffcbb98dd16385
N1R0 <85079850+ncrcodes@users.noreply.github.com>
2021-07-06 22:48:11 +0200

    Delete src/bot directory

--- src/bot/events/music/error.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function(client, message, err) {
-    var { MessageEmbed } = require('discord.js');
-    message.channel.send(
-        new MessageEmbed()
-        .setColor('RED')
-        .setDescription(err)
-    )
-}
