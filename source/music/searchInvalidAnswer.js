abb677ae69881508d899dac5bbe1db70534bb5e3
N1R0 <85079850+N1R0tka@users.noreply.github.com>
2021-09-19 14:34:50 -0700

    Update searchInvalidAnswer.js

--- source/music/searchInvalidAnswer.js
+++ source/music/searchInvalidAnswer.js
@@ -7,7 +7,7 @@ const { Client, MessageEmbed, Message } = require('discord.js');
  */
 
 module.exports = async(client, message) => {
-    let lang = require('../slashCommands/play').guildLANG || require('../commands/play').guildLANG;
+    let lang = require('../slashCommands/play').guildLANG;
     if (lang == "en") {
         message.channel.send({ content: ":x: | **Worang Input!.**", embeds: [], allowedMentions: false });
     } else if (lang == "ar") {
