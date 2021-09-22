a3a37b95e830ca73737a1cb6ec57d4b553de0d2c
N1R0 <85079850+N1R0tka@users.noreply.github.com>
2021-09-22 17:13:04 +0200

    Update index.js

--- source/index.js
+++ source/index.js
@@ -6,7 +6,7 @@ const commands = new Collection();
 const SoundCloudPlugin = require('@distube/soundcloud');
 const SpotifyPlugin = require('@distube/spotify');
 const client = new Client({
-    intents: 32767,
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING],
     partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"],
     allowedMentions: {
         parse: ['users', 'roles'],
