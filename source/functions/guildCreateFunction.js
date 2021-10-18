e113e837efb59eb0aaba3409a88410812f4dd413
N1R0 <85079850+N1R0tka@users.noreply.github.com>
2021-10-18 13:55:42 -0700

    Delete source directory

--- source/functions/guildCreateFunction.js
+++
@@ -1,8 +0,0 @@
-async function guildCreateFunction(guild, db) {
-    db.set(`Settings_${guild.id}`, {
-        prefix: require('../../config/bot.json').mainPrefix,
-        lang: require('../../config/bot.json').mainLang
-    });
-};
-
-module.exports.get = guildCreateFunction;
